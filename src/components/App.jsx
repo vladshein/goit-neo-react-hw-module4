518c966eadc051137c2f9ab83d230652d9e070a4
vladshein <vlshein1@gmail.com>
2025-05-14 11:49:22 +0300

    add solution to HW4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,87 @@
 import { useState, useEffect } from "react";
-import SearchBar from "./SearchBar/SearchBar";
-import FormikForm from "./FormikForm/FormikForm";
-import ContactList from "./ContactList/ContactList";
-import initialList from "../initiallist.json";
 
-import css from "./App.module.css";
+import SearchBar from "./SearchBar/SearchBar";
 import searchbar from "./SearchBar/SearchBar.module.css";
+import css from "./App.module.css";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import { fetchPhotos } from "./api/api";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
+import Loader from "./Loader/Loader";
+import ImageModal from "./ImageModal/ImageModal";
 
 const App = () => {
-  const [list, setList] = useState(() => {
-    const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : initialList;
-  });
+  const [query, setQuery] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [modal, setModalOpen] = useState(false);
+  const [selectedPhoto, setselectedPhoto] = useState(null);
 
-  const [filter, setFilter] = useState("");
+  const handleQuery = inputQuery => {
+    setQuery(inputQuery);
+    setPage(1);
+  };
+
+  const handleLoadMore = async () => {
+    setPage(page + 1);
+  };
+
+  const openModal = photo => {
+    setselectedPhoto(photo);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setselectedPhoto(null);
+  };
 
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(list));
-  }, [list]);
+    if (!query) return;
+    const getPhotos = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const { data } = await fetchPhotos(query, page);
+        console.log("Data is", data);
+        setPhotos(prev =>
+          page === 1 ? [...data.results] : [...prev, ...data.results]
+        );
+      } catch (error) {
+        setError(true);
+        setPhotos([]);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const addContact = newContact => {
-    setList(prevList => {
-      return [...prevList, newContact];
-    });
-  };
-
-  const deleteTask = contactId => {
-    setList(prevList => {
-      return prevList.filter(contact => contact.id !== contactId);
-    });
-  };
-
-  const visibleContacts = list.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+    getPhotos();
+  }, [query, page]);
 
   return (
     <div className={css.app}>
-      <h1>Phonebook</h1>
-      <FormikForm onAdd={addContact} />
       <SearchBar
         className={searchbar.searchbar}
-        value={filter}
-        onFilter={setFilter}
+        onSubmit={handleQuery}
+        isDisabled={loading}
       />
-      <ContactList list={visibleContacts} onDelete={deleteTask} />
+      {error && <ErrorMessage />}
+      {loading && <Loader />}
+      {!photos.length && !loading ? (
+        <h2>No results, please enter new query.</h2>
+      ) : (
+        <ImageGallery photos={photos} openModal={openModal} />
+      )}
+
+      {photos.length > 0 && <LoadMoreBtn handleMore={handleLoadMore} />}
+      <ImageModal
+        modalOpen={modal}
+        closeModal={closeModal}
+        selectedPhoto={selectedPhoto}
+      />
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,37 @@
-import { useId } from "react";
 import css from "./SearchBar.module.css";
+import toast, { Toaster } from "react-hot-toast";
 
-const SearchBar = ({ value, onFilter }) => {
-  const searchId = useId();
+const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = evt => {
+    evt.preventDefault();
+
+    const form = evt.target;
+    const photos = form.elements.input.value;
+
+    if (photos.trim() === "") {
+      toast.error("Please enter your query.");
+      return;
+    }
+
+    onSubmit(photos);
+    form.reset();
+  };
 
   return (
-    <div>
-      <label htmlFor={searchId}> Find contacts by name</label>
-      <input
-        className={css.searchbar}
-        type="text"
-        value={value}
-        onChange={e => onFilter(e.target.value)}
-        id={searchId}
-      />
-      {/* <p>{inputValue}</p> */}
-    </div>
+    <header>
+      <Toaster />
+      <form className={css.searchbar} onSubmit={handleSubmit}>
+        <input
+          className={css.forminput}
+          name="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
   );
 };
 
